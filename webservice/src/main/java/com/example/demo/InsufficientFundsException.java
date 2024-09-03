d4fe683ffcc29dc4cb78f312a8461755157af5c7
momo945 <79753313+momo945@users.noreply.github.com>
2024-09-03 20:36:27 +0000

    Delete webservice directory

--- webservice/src/main/java/com/example/demo/InsufficientFundsException.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo;
-
-
-public class InsufficientFundsException extends RuntimeException {
-    public InsufficientFundsException(String message) {
-        super(message);
-    }
-}
--- webservice/src/main/java/com/example/demo/ResourceNotFoundException.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo;
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
